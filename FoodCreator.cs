aea21b0344c575e86b8d116fc275d5dc45e47f56
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-08-26 17:19:40 +0300

    add FoodCreator

---
+++ FoodCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class FoodCreator
+    {
+        int mapWidth;
+        int mapWeight;
+        char sym;
+
+        Random random = new Random();
+        
+        public FoodCreator(int mapWidth, int mapWeight, char sym)
+        {
+            this.mapWidth = mapWidth;
+            this.mapWeight = mapWeight;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth - 2);
+            int y = random.Next(2, mapWeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
---
+++ HorizontalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class HorizontalLine : Figure
+    {
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+    }
+}
